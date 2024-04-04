f83143f11ced504f0ef954af722de02a4b722add
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2024-04-04 09:44:46 -0400

    more handle packet stuff

--- pwospf_stub/sr_router.c
+++ pwospf_stub/sr_router.c
@@ -310,10 +310,27 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t* packet, char* ip_interface, u
     	uint8_t incoming_ip_proto = ip_packet->ip_p;
     	if (incoming_ip_proto == ip_protocol_udp) {
     		printf("Is UDP packet.\n");
-    		/* TODO: check udp checksum and length */
+    		/* TODO: check udp checksum and length. how much to add to get udp header? */
     		sr_udp_hdr_t* incoming_udp_packet = (sr_udp_hdr_t*) (ip_packet + sizeof(sr_ip_hdr_t));
     		
-    		
+    		/* TODO: Is packet RIP or other UDP? */
+    		uint16_t port_dst = incoming_udp_packet->port_dst;
+    		if (port_dst == 520) {
+    			printf("Is RIP packet.\n");
+    			
+    			sr_rip_pkt_t* incoming_rip_packet = (sr_rip_pkt_t*)(incoming_udp_packet + sizeof(sr_udp_hdr_t));
+    			uint8_t command = incoming_rip_packet->command;
+    			if (command == 1) {
+    				printf("RIP response.\n");
+    				/* TODO: update routing table */
+    			} else {
+    				printf("RIP request.\n");
+    				/* TODO: send RIP response */
+    			}
+    		} else {
+    			printf("Is not RIP packet, sending exception.\n");
+    			send_icmp_reply(sr, 3, 3, packet, (struct sr_if*)ip_interface, 0);
+    		}
     		
     	} else {
     		printf("Is TCP, sending exception.\n");
